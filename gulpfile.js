e12832de09d63041e29e6226579bfcc14850dd58
Artem Korolev <artem256korolev@gmail.com>
2024-11-29 10:40:56 +0300

    my gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 "use strict";
 
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'))
-const rename = require('gulp-rename')
-const cleanCSS = require('gulp-clean-css')
-const babel = require('gulp-babel')
-const uglify = require('gulp-uglify')
-const concat = require('gulp-concat')
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const htmlmin = require('gulp-htmlmin');
+const newer = require('gulp-newer');
+const browsersync = require('browser-sync').create();
 
 // Пути до папки dist
 const path = {
@@ -17,44 +21,128 @@ const path = {
     scripts: {
         src: 'src/scripts/**/*.js',
         dest: 'dist/js/'
+    },
+    images: {
+        src: 'src/images/**/*',
+        dest: 'dist/images'
+    },
+    html: {
+        src: 'src/*.html',
+        dest: 'dist'
     }
 }
 
+function handleError(err) {
+    console.error(err.toString());
+    this.emit('end');
+}
+
+// Задача для минимализации HTML
+async function html() {
+    const size = (await import('gulp-size')).default;
+
+    return gulp.src(path.html.src)
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(size())
+        .pipe(gulp.dest(path.html.dest))
+        .pipe(browsersync.stream());
+}
+
 // Задача для обработки стилей
-function styles(){
+async function styles() {
+    const autoprefixer = await import('gulp-autoprefixer');
+    const size = (await import('gulp-size')).default;
+
     return gulp.src(path.styles.src)
         .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS())
+        .pipe(autoprefixer.default({
+            cascade: false,
+            overrideBrowserslist: ["> 0.5%", "last 3 versions"]
+        }))
+        .pipe(cleanCSS({
+            level: 2
+        }))
         .pipe(rename({
             basename: 'main',
             suffix: '.min'
         }))
+        .pipe(size())
         .pipe(gulp.dest(path.styles.dest))
+        .pipe(browsersync.stream());
 }
 
 // Задача для обработки скриптов
-function scripts(){
-    return gulp.src(path.scripts.src, {
-        sourcemaps: true
-    })
-    .pipe(babel())
-    .pipe(uglify())
-    .pipe(concat('main.min.js'))
-    .pipe(gulp.dest(path.scripts.dest))
+async function scripts() {
+    const size = (await import('gulp-size')).default;
+
+    return gulp.src(path.scripts.src, { sourcemaps: true })
+        .pipe(sourcemaps.init())
+        .pipe(babel({ presets: ['@babel/env'] }))
+        .pipe(uglify().on('error', handleError))
+        .pipe(concat('main.min.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(size())
+        .pipe(gulp.dest(path.scripts.dest))
+        .pipe(browsersync.stream());
 }
 
+// Задача для сжатия изображений
+async function img() {
+    const imagemin = (await import('gulp-imagemin')).default;
+    const mozjpeg = (await import('imagemin-mozjpeg')).default;
+    const pngquant = (await import('imagemin-pngquant')).default;
+    const webp = (await import('imagemin-webp')).default;
+    const gifsicle = (await import('imagemin-gifsicle')).default;
+    const svgo = (await import('imagemin-svgo')).default;
+    const size = (await import('gulp-size')).default;
 
-// Отслеживание измеений
-function watch(){
-    gulp.watch(path.styles.src, styles)
-    gulp.watch(path.scripts.src, scripts)
+    return gulp.src(path.images.src,  {
+        encoding: false
+    })
+        .pipe(newer(path.images.dest))
+        .pipe(imagemin([
+            gifsicle({interlaced: true}),
+            mozjpeg({quality: 75, progressive: true}),
+            pngquant({ quality: [0.65, 0.80], speed: 4 }),
+            webp({ quality: 75 }),
+            svgo({
+                plugins: [
+                    {
+                        name: 'removeViewBox',
+                        active: true
+                    },
+                    {
+                        name: 'cleanupIDs',
+                        active: false
+                    }
+                ]
+            })
+        ]))
+        .pipe(size())
+        .pipe(gulp.dest(path.images.dest));
+}
+
+// Отслеживание изменений
+function watch() {
+    browsersync.init({
+        server: {
+            baseDir: './dist/'
+        }
+    })
+    gulp.watch(path.html.dest).on('change', browsersync.reload)
+    gulp.watch(path.html.src, html);
+    gulp.watch(path.styles.src, styles);
+    gulp.watch(path.scripts.src, scripts);
+    gulp.watch(path.images.src, img);
 }
 
 // Задача билд
-const build = gulp.series(gulp.parallel(styles, scripts), watch)
+const build = gulp.series(html, gulp.parallel(styles, scripts, img), watch);
 
-exports.styles = styles
-exports.scripts = scripts
-exports.watch = watch
-exports.build = build
-exports.default = build
+exports.styles = styles;
+exports.scripts = scripts;
+exports.img = img;
+exports.html = html;
+exports.watch = watch;
+exports.build = build;
+exports.default = build;
